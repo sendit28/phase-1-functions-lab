c4ce5dbd09bfe691907c1b25011c05ac42201bdf
+ <+>
2022-07-17 11:40:09 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function distanceFromHqInBlocks(blockNum) {
     // blockNum was below 42, so subtract from 42, 42- blockNum
     if (blockNum < 42) {
         return 42 - blockNum
-        
     } else {
         return blockNum - 42
     }    
@@ -15,14 +14,12 @@ function distanceFromHqInFeet(blockNum) {
 function distanceTravelledInFeet(start, destination) {
     let startBlock = start 
     let endBlock = destination 
-    if (endBlock < startBlock)
-    {  
+    if (endBlock < startBlock) {  
         return (startBlock - endBlock) * 264
     } else {
         return (endBlock - startBlock) * 264
     }  
 }
-
 function calculatesFarePrice(start, destination) {
     //first 400 free 
     const distance = distanceTravelledInFeet(start, destination)
@@ -32,15 +29,12 @@ function calculatesFarePrice(start, destination) {
 //400 to 2000  .02 * feet
     else if (distance > 400 && distance <= 2000) {
         return (distance - 400) * .02
-    }
 //flatfare 2000 < 2500
-    else if (distance > 2000 && distance <= 2500) {
+    } else if (distance > 2000 && distance <= 2500) {
         return 25
-
     } else {
         return 'cannot travel that far'
     }
-   
-//> 2500 return 'cannot travel that far'
-  }
+}   
+
     
